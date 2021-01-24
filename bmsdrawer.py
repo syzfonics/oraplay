40c1e14f1f51a0d5a94b71a977c65e25af247426
syzfonics <cross.advice2000@gmail.com>
2021-01-25 03:08:15 +0900

    ノートの描画方法を別クラスに変更

--- bmsdrawer.py
+++ bmsdrawer.py
@@ -96,8 +96,68 @@ class Canvas():
         self.height = height
         self.barlist = barlist
 
+class NoteDrawer():
+    def __init__(self, bar_height: int, key_size: KeySize=ModeSevenKeySize(),
+        color: List=( COLOR_RED, COLOR_WHITE, COLOR_BLUE, COLOR_WHITE, COLOR_BLUE, COLOR_WHITE, COLOR_BLUE, COLOR_WHITE )):
+        self.drawer = None
+        self.bar_height = bar_height
+        self.key_size = key_size
+        self.color = color
+
+    def set_drawer(self, drawer):
+        self.drawer = drawer
+
+    def __draw_note_implement(self, note, order, pos):
+        x_start = pos[0]
+        x_end = x_start + self.key_size.get_widths()[order] - 1
+        y_start = pos[1] + int((1 - note.timing) * self.bar_height) - self.key_size.get_height() - 1
+        y_end = y_start + self.key_size.get_height()
+        self.drawer.rectangle((x_start, y_start, x_end, y_end), fill=self.color[order])
+
+    def draw_note(self, notes, order, pos) -> None:
+        for note in notes:
+            self.__draw_note_implement(note, order, pos)
+
+    def __draw_note_ln_layer_start(self, note, order, pos):
+        x_start = pos[0] + 3
+        x_end = x_start + self.key_size.get_widths()[order] - 1 - 6
+        y_start = pos[1] + int((1 - note.timing) * self.bar_height) - self.key_size.get_height() - 1
+        y_end = y_start + self.key_size.get_height() - 2
+        self.drawer.rectangle((x_start, y_start, x_end, y_end), fill=COLOR_YELLOW)
+
+    def __draw_note_ln_layer_end(self, note, order, pos):
+        x_start = pos[0] + 3
+        x_end = x_start + self.key_size.get_widths()[order] - 1 - 6
+        y_start = pos[1] + int((1 - note.timing) * self.bar_height) - self.key_size.get_height() + 1
+        y_end = y_start + self.key_size.get_height() - 2
+        self.drawer.rectangle((x_start, y_start, x_end, y_end), fill=COLOR_YELLOW)
+
+    def __draw_note_ln_layer(self, note, order, pos):
+        x_start = pos[0] + 3
+        x_end = x_start + self.key_size.get_widths()[order] - 1 - 6
+        y_start = pos[1] + int((1 - note.end) * self.bar_height) - 1
+        y_end = pos[1] + int((1 - note.start) * self.bar_height) - self.key_size.get_height() - 1
+        if note.is_start is True:
+            y_end -= 1
+        else:
+            y_end += self.key_size.get_height()
+        if note.is_end is True:
+            y_start -= 1
+        self.drawer.rectangle((x_start, y_start, x_end, y_end), fill=COLOR_YELLOW)
+
+    def draw_lnnote(self, notes, order, pos):
+        for note in notes:
+            if isinstance(note, bms.LNStart):
+                self.__draw_note_implement(note, order, pos)
+                self.__draw_note_ln_layer_start(note, order, pos)
+            if isinstance(note, bms.LNEnd):
+                self.__draw_note_implement(note, order, pos)
+                self.__draw_note_ln_layer_end(note, order, pos)
+            if isinstance(note, bms.LN):
+                self.__draw_note_ln_layer(note, order, pos)
+
 class BMSImage():
-    def __init__(self, data: Union[bms.BMS, List[bms.BarInfo]], keymode: KeyMode=KeyMode.mode7key, keysize: KeySize=ModeSevenKeySize(),
+    def __init__(self, data: Union[bms.BMS, List[bms.BarInfo]], style=None, keymode: KeyMode=KeyMode.mode7key, keysize: KeySize=ModeSevenKeySize(),
         line_width: int=1, bar_height: int=200, canvas_height: int=1000, width_offset: int=20, height_offset: int=50):
         if isinstance(data, bms.BMS):
             self.data = data.bars
@@ -113,6 +173,10 @@ class BMSImage():
         self.canvas_height = canvas_height
         self.width_offset = width_offset
         self.height_offset = height_offset
+        if style is not None:
+            self.style = style
+        else:
+            self.style = NoteDrawer(bar_height, keysize)
 
     def __bar_width(self) -> int:
         return sum(self.keysize.get_widths()) + self.info_width + self.line_width * 10
@@ -177,6 +241,7 @@ class BMSImage():
 
     def __draw_notes(self, modify: List[int]=[0, 1, 2, 3, 4, 5, 6]):
         dr = ImageDraw.Draw(self.image)
+        self.style.set_drawer(dr)
         cursor = [ self.width_offset, self.canvas.height - self.height_offset - 1 ]
         for line in self.canvas.barlist:
             for b in line:
@@ -192,72 +257,20 @@ class BMSImage():
                         fill=(0, 255, 0), width=self.line_width*2)
                     dr.text((note_cursor[0] + 2, y_bpm - 11), text=str(bpm.bpm), anchor='rs', fill=(0, 255, 0))
 
-                # draw notes
-
-                def __draw_note(note: bms.Note, order: int, pos: Tuple[int, int], color: Tuple) -> None:
-                    x_start = pos[0]
-                    x_end = x_start + self.keysize.get_widths()[order] - 1
-                    y_start = pos[1] + int((1 - note.timing) * bar_height) - self.keysize.get_height() - 1
-                    y_end = y_start + self.keysize.get_height()
-                    dr.rectangle((x_start, y_start, x_end, y_end), fill=color)
-
-                def __draw_note_ln_layer_start(note: bms.Note, order: int, pos: Tuple[int, int], color: Tuple) -> None:
-                    x_start = pos[0] + 3
-                    x_end = x_start + self.keysize.get_widths()[order] - 1 - 6
-                    y_start = pos[1] + int((1 - note.timing) * bar_height) - self.keysize.get_height() - 1
-                    y_end = y_start + self.keysize.get_height() - 2
-                    dr.rectangle((x_start, y_start, x_end, y_end), fill=color)
-                    pass
-
-                def __draw_note_ln_layer_end(note: bms.Note, order: int, pos: Tuple[int, int], color: Tuple) -> None:
-                    x_start = pos[0] + 3
-                    x_end = x_start + self.keysize.get_widths()[order] - 1 - 6
-                    y_start = pos[1] + int((1 - note.timing) * bar_height) - self.keysize.get_height() + 1
-                    y_end = y_start + self.keysize.get_height() - 2
-                    dr.rectangle((x_start, y_start, x_end, y_end), fill=color)
-                    pass
-
-                def __draw_ln_notes(notes: list[bms.LNBase], order: int, pos: Tuple[int, int], color: Tuple, color_ln: Tuple) -> None:
-                    for note in notes:
-                        if isinstance(note, bms.LNStart):
-                            __draw_note(note, order, pos, color)
-                            __draw_note_ln_layer_start(note, order, pos, color_ln)
-                            continue
-                        if isinstance(note, bms.LNEnd):
-                            __draw_note(note, order, pos, color)
-                            __draw_note_ln_layer_end(note, order, pos, color_ln)
-                            continue
-                        if isinstance(note, bms.LN):
-                            x_start = pos[0] + 3
-                            x_end = x_start + self.keysize.get_widths()[order] - 1 - 6
-                            y_start = pos[1] + int((1 - note.end) * bar_height) - 1
-                            y_end = pos[1] + int((1 - note.start) * bar_height) - self.keysize.get_height() - 1
-                            if note.is_start is True:
-                                y_end -= 1
-                            else:
-                                y_end += self.keysize.get_height()
-                                pass
-                            if note.is_end is True:
-                                y_start -= 1
-                            dr.rectangle((x_start, y_start, x_end, y_end), fill=color_ln)
-
-                def __move_cursor(cr, order):
+                def move_cursor(cr, order):
                     cr += self.keysize.get_widths()[order]
                     cr += self.line_width
                     return cr
 
                 note_cursor[0] += self.line_width * 2
                 note_cursor[0] += self.info_width
-                for scratch in b.notes[0]:
-                    __draw_note(scratch, 0, note_cursor, COLOR_RED)
-                __draw_ln_notes(b.lnnotes[0], 0, note_cursor, COLOR_RED, COLOR_YELLOW)
+                self.style.draw_note(b.notes[0], 0, note_cursor)
+                self.style.draw_lnnote(b.lnnotes[0], 0, note_cursor)
 
-                color_index = [ COLOR_WHITE, COLOR_BLUE, COLOR_WHITE, COLOR_BLUE, COLOR_WHITE, COLOR_BLUE, COLOR_WHITE ]
                 for i, m in enumerate(modify):
-                    note_cursor[0] = __move_cursor(note_cursor[0], i)
-                    for n in b.notes[m + 1]:
-                        __draw_note(n, i + 1, note_cursor, color_index[i])
-                    __draw_ln_notes(b.lnnotes[m + 1], i + 1, note_cursor, color_index[i], COLOR_YELLOW)
+                    note_cursor[0] = move_cursor(note_cursor[0], i)
+                    self.style.draw_note(b.notes[m + 1], i + 1, note_cursor)
+                    self.style.draw_lnnote(b.lnnotes[m + 1], i + 1, note_cursor)
 
                 cursor[1] -= bar_height
             cursor[0] += self.__bar_width()
