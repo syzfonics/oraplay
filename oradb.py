c3a3f8d8fbdf3bdd3aa247dcbe7d2f4ed59988bc
syzfonics <cross.advice2000@gmail.com>
2021-02-05 21:09:40 +0900

    oradbにてmd5も扱えるように修正

--- oradb.py
+++ oradb.py
@@ -1,10 +1,19 @@
 import sqlite3
+from enum import Enum, auto
+
+class HashType(Enum):
+    md5 = auto()
+    sha256 = auto()
 
 class SongDB():
     def __init__(self, path: str):
         self.db = sqlite3.connect(path)
 
-    def get_file_path(self, sha256: str):
-        c = self.db.execute("SELECT path FROM song WHERE sha256='{}'".format(sha256))
+    def get_file_path(self, hash: str, hash_type: HashType=HashType.sha256):
+        if hash_type == HashType.sha256:
+            hash_str = "sha256"
+        elif hash_type == HashType.md5:
+            hash_str = "md5"
+        c = self.db.execute("SELECT path FROM song WHERE {}='{}'".format(hash_str, hash))
         data = c.fetchone()
         return data[0]
